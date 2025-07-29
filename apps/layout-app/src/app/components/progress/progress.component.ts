343ed42febceb5491715a0a0cf67614e5b284b45
JCM <jcl.maquinay@gmail.com>
2025-07-29 16:55:43 +0200

    Start add flag and dictionaries

---
+++ apps/layout-app/src/app/components/progress/progress.component.ts
@@ -0,0 +1,17 @@
+import { Component, computed, input } from '@angular/core';
+
+
+@Component({
+    selector: 'app-progress',
+    imports: [],
+    templateUrl: './progress.component.html',
+    styleUrl: './progress.component.scss'
+})
+export class ProgressComponent {
+  readonly value = input.required<number>();
+
+  readonly max = input.required<number>();
+
+  readonly ratio = computed(() => this.value() / this.max());
+
+}
